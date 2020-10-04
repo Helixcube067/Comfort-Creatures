cdf4a9331434957f14ce6e4f3fa7490dc8020b18
Andrew Welby <apw50@pitt.edu>
2020-10-04 12:28:57 -0500

    6 hours out about

---
+++ Comfort Creatures Game/Assets/FollowMousePos.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMousePos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector3 mousePos = Input.mousePosition;
+		
+		mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+		mousePos.z = 0;
+		transform.position = mousePos;
+
+	}
+}
